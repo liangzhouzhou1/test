4ec78622acfe53da3e9f7ecb83a5ad8564f80e41
liangzhouzhou1 <liangzhouzhou1@163.com>
2016-10-02 17:05:02 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package NA;
+
+import java.util.*;
+
+public class Ch1 {
+
+	public static void main(String[] args) {
+		System.out.println();
+		List<String> arr = new ArrayList<>();
+		arr.add("hello");
+		arr.add("world");
+		arr.forEach(System.out::println);
+	}
+
+}
